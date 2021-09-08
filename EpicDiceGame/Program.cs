7590ca6208c376977b6284c81bfb516973e19c34
Opilane TTHK <opilane@tthk.ee>
2021-09-08 12:33:43 +0300

    add random

---
+++ EpicDiceGame/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EpicDiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mängus osaleb kaks mängijat;
+            //iga mängija viskab täringut (1-6)
+            //programm kuvab nende visete tulemused ja kuvab ka, kes on võitja
+            //-ehk kes viskas suuremat
+
+            {
+                Random rnd = new Random();
+
+                Console.WriteLine("Mängija 1 sisesta oma nimi");
+                string no1 = Console.ReadLine();
+                Console.WriteLine("Mängija 2 sisesta oma nimi");
+                string no2 = Console.ReadLine();
+
+                int MyRandomNumber = rnd.Next(1, 7);
+                Console.WriteLine($"{no1},veeretas {MyRandomNumber}.");
+                int MyRandom = rnd.Next(1, 7);
+                Console.WriteLine($"no2 ,veeretas" {Myrandom });
+            
+            
+            
+            }
+
+                   
+        
+        } 
+    }
+}
---
+++ RandomNumberGenerators/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace RandomNumberGenerators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int MyRandomNumber = rnd.Next(1, 11);
+
+            //Programm genereerib juhuslikku numbrit 1 kuni 10
+            //Programm kontrollib ,kas genereeritud number on 
+            //suurem kui 5
+            //kui number on suurem ,kui 5, siis konsool kuvab 
+            //juhuslik number on (MyRandomNumber) ,see on väiksem
+            //kui 5";
+            //kui number on 5, siis konsool kuvab "juhuslik number on 5"
+
+            Console.WriteLine(MyRandomNumber);
+
+            if (MyRandomNumber > 5)
+
+            {
+                Console.WriteLine("Number on suurem kui 5");
+            }
+           else if (MyRandomNumber< 5)
+            {
+                Console.WriteLine("Number on väiksem kui 5");
+            }
+            else
+            {
+                Console.WriteLine("Juhuslik Number ongi 5");
+            }       
+       }
+
+    }
+}
